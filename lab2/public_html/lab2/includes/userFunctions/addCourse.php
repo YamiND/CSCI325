9f350233aff37cde3b2470459f17aceee8842249
Tyler Postma <tpostma@lssu.edu>
2017-03-08 10:32:32 -0500

    Can now add courses

--- lab2/public_html/lab2/includes/userFunctions/addCourse.php
+++ lab2/public_html/lab2/includes/userFunctions/addCourse.php
@@ -6,81 +6,76 @@ sec_session_start(); // Our custom secure way of starting a PHP session.
 
 if (login_check($mysqli) == true) 
 {
-	addCourse();
+	addCourse($mysqli);
 }
 else
 {
-	echo '<pre>';
-var_dump($_SESSION);
-echo '</pre>';
-
    	$_SESSION['fail'] = 'Course Creations Failed, invalid permissions';
-//   	header('Location: ../../pages/adduser');
-
-	return;
+   	header('Location: ../../pages/addcourse');
 }
 
-function addCourse()
+function addCourse($mysqli)
 {
+	$duplicate = false;
+
 	if (isset($_POST['courseNumber'], $_POST['courseTitle'], $_POST['courseDescription'], $_POST['courseYear'], $_POST['courseFaculty'])) 
 	{
-		 foreach($_POST as $key => $value)
-                {   
-                        //strip slashes
-                        $value=stripslashes($value);
-    
-                        //strip html shit
-                        $value = trim(preg_replace('/ +/', ' ', preg_replace('/[^A-Za-z0-9 ]/', ' ', urldecode(html_entity_decode(strip_tags($value))))));
-    
-                        //trim spaces from right end of string
-                        $value = rtrim($value);
-    
-                        //trim spacs from left end of string
-                        $value = ltrim($value);
-                } 
-	
     	$courseNumber = $_POST['courseNumber'];
 		$courseTitle = $_POST['courseTitle'];
 		$courseDescription = $_POST['courseDescription'];
 		$courseYear = $_POST['courseYear'];
 		$courseFaculty = $_POST['courseFaculty'];
 
- 		$fileName = COURSECSV;
-   		$newArray = array_map('str_getcsv', file($fileName));
-		$success = false;
-
-        for ($i = 0; $i < count($newArray); $i++)
-        {   
-            if ($newArray[$i][1] == $courseNumber) 
-            {   
-                    $success = false;
-					break;
-            }   
-        }    	
-
-		if ($success)
+		if ($stmt = $mysqli->prepare("SELECT courseID FROM courses WHERE courseName = ?"))
 		{
-    		$error = "Course already exists"; 
-	   	   	header('Location: ../../pages/error?error=' . $error);
+			$stmt->bind_param('s', $courseTitle);
+			$stmt->store_result();
+			if ($stmt->execute())
+			{
+				if ($stmt->num_rows > 0)
+				{
+					$duplicate = true;
+				}
+				else
+				{
+					$duplicate = false;
+				}
+			}
+			$stmt->close();
+		}
+
+		if (!$duplicate)
+		{
+			if ($stmt = $mysqli->prepare("INSERT INTO courses (courseCode, courseName, courseDescription, courseYear, courseFacultyID) VALUES (?, ?, ?, ?, ?)"))
+			{
+				$stmt->bind_param('ssssi', $courseNumber, $courseTitle, $courseDescription, $courseYear, $courseFaculty);
+
+				if ($stmt->execute())
+				{
+					$_SESSION['success'] = "Course added";
+			   	   	header('Location: ../../pages/addcourse');
+				}
+				else
+				{
+    				$error = "Can not add course, database insert faliure"; 
+			   	   	header('Location: ../../pages/error?error=' . $error);
+				}
+			}
+			else
+			{
+    			$error = "Can not add course, database insert query fail"; 
+		   	   	header('Location: ../../pages/error?error=' . $error);
+			}
 		}
 		else
 		{
-			$fileName = COURSECSV;
-
-			$handle = fopen($fileName, "a");
-
-			fputcsv($handle, array($courseNumber, $courseTitle, $courseDescription, $courseYear, $courseFaculty));
-			
-			fclose($handle);
-
-			$_SESSION['success'] = "Course created";
-
-   	   		header('Location: ../../pages/addcourse');
+    		$error = "Can not add course, already exists"; 
+	   	   	header('Location: ../../pages/error?error=' . $error);
 		}
     }
 	else
 	{
-    		$error = "Course already exists"; 
+    		$error = "Can not add course, data not sent"; 
 	   	   	header('Location: ../../pages/error?error=' . $error);
 	}
 }
